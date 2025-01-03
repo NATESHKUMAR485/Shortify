6792e4cf5f95aad94da3b331cbbdb9b92a70e762
Natesh Kumar <74709189+NATESHKUMAR485@users.noreply.github.com>
2025-01-03 18:37:55 +0500

    removed the code of the shortenid/page.js

--- shortify/src/app/shortenId/page.js
+++ shortify/src/app/shortenId/page.js
@@ -1,24 +1,24 @@
-import { notFound } from 'next/navigation';
-import connectDB from '../../lib/mongodb';
-import URL from '../../models/URL';
+// import { notFound } from 'next/navigation';
+// import connectDB from '../../lib/mongodb';
+// import URL from '../../models/URL';
 
-export const dynamic = 'force-dynamic'; // Prevents static prerendering
+// export const dynamic = 'force-dynamic'; // Prevents static prerendering
 
-export default async function ShortIdPage({ params }) {
-  const { shortId } = params;
+// export default async function ShortIdPage({ params }) {
+//   const { shortId } = params;
 
-  await connectDB();
+//   await connectDB();
 
-  const url = await URL.findOne({ shortId });
+//   const url = await URL.findOne({ shortId });
 
-  if (!url) {
-    notFound(); // Show a 404 page if no URL is found
-  }
+//   if (!url) {
+//     notFound(); // Show a 404 page if no URL is found
+//   }
 
-  return {
-    redirect: {
-      destination: url.originalUrl,
-      permanent: false,
-    },
-  };
-}
+//   return {
+//     redirect: {
+//       destination: url.originalUrl,
+//       permanent: false,
+//     },
+//   };
+// }
