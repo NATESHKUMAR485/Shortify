e13222fe7b9906d225783b38e7dd6989c975f5b8
Natesh Kumar <74709189+NATESHKUMAR485@users.noreply.github.com>
2025-01-03 06:03:36 +0500

    Update page.js

--- shortify/src/app/shortenId/page.js
+++ shortify/src/app/shortenId/page.js
@@ -1,29 +1,4 @@
 import { notFound } from 'next/navigation';
-// import connectDB from '../../lib/mongodb';
-// import URL from '../../models/URL';
-
-// export default async function ShortIdPage({ params }) {
-//   const { shortId } = params;
-
-//   await connectDB();
-
-//   const url = await URL.findOne({ shortId });
-
-//   if (!url) {
-//     notFound(); // Show 404 if shortId is not found
-//     return;
-//   }
-
-//   // Redirect to the original URL
-//   if (typeof window !== 'undefined') {
-//     window.location.href = url.originalUrl;
-//     console.log('URL not found  to original URL');
-//   }
-
-//   return null; // Nothing is rendered on the server
-// }
-
-
 import connectDB from '../../lib/mongodb';
 import URL from '../../models/URL';
 
@@ -35,8 +10,14 @@ export default async function ShortIdPage({ params }) {
   const url = await URL.findOne({ shortId });
 
   if (!url) {
-    return notFound(); // Show 404 page
+    return notFound(); // Show 404 if shortId is not found
   }
+
+  // Redirect to the original URL
+//   if (typeof window !== 'undefined') {
+//     window.location.href = url.originalUrl;
+//     console.log('URL not found  to original URL');
+//   }
 
   // Redirect to the original URL
   return {
@@ -46,3 +27,5 @@ export default async function ShortIdPage({ params }) {
     },
   };
 }
+
+
